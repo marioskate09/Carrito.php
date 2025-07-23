6abbcffaf67175065bb774bb679769ac5957644f
marioskate09 <andresxx_123@hotmail.com>
2025-07-23 14:26:23 -0500

    Change connection database

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-carrito en php
+<?php
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "";
+
+$conexion = new mysqli($server, $user, $pass, $db);
+
+if ($conexion->connect_errno)
+
+ ?>
